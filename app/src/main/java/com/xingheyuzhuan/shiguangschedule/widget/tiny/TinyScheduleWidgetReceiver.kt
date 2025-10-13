4938e405df538b33893c59c2983c0bc8f091942f
YIKE <2708503202@qq.com>
2025-10-13 20:21:27 +0800

    feat: 重构小组件样式

--- app/src/main/java/com/xingheyuzhuan/shiguangschedule/widget/tiny/TinyScheduleWidgetReceiver.kt
+++ app/src/main/java/com/xingheyuzhuan/shiguangschedule/widget/tiny/TinyScheduleWidgetReceiver.kt
@@ -1,23 +1,21 @@
-// DoubleDaysScheduleWidgetReceiver.kt
-package com.xingheyuzhuan.shiguangschedule.widget.double_days
+package com.xingheyuzhuan.shiguangschedule.widget.tiny
 
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import com.xingheyuzhuan.shiguangschedule.widget.WorkManagerHelper
 
-class DoubleDaysScheduleWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = DoubleDaysScheduleWidget()
+class TinyScheduleWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TinyScheduleWidget()
 
     override fun onEnabled(context: Context) {
         super.onEnabled(context)
-        // 当第一个小组件实例被添加时，调度定期任务
         WorkManagerHelper.schedulePeriodicWork(context)
     }
 
     override fun onDisabled(context: Context) {
         super.onDisabled(context)
-        // 当最后一个小组件实例被移除时，取消所有定期任务
         WorkManagerHelper.cancelAllWork(context)
     }
 }
+
